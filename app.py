21c456aebcc1ab4ab8ff3a2d8f9cbfb6efe36021
Charlie <charliebailey24@gmail.com>
2023-11-07 19:12:28 -0800

    forgot / in route

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('db_test')
+@app.route('/db_test')
 def db_test():
     conn = psycopg2.connect("postgres://software_dev_lab_10_db_user:iajeORETZpYisK3P9WSiCJVGIQvVxhE0@dpg-cl5fk7d6fh7c73emhpo0-a/software_dev_lab_10_db")
     conn.close()
